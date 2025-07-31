17d1d6d17efa2cecea3ffee372ed14facd73e7b1
Madhushree B <boylemadhushree@gmail.com>
2025-07-31 11:33:09 +0200

    whileTap transition changes

--- src/Component/Quotes.jsx
+++ src/Component/Quotes.jsx
@@ -52,7 +52,7 @@ export default function Quotes() {
               scale: 1.2,
               transition: { duration: 0.1 },
             }}
-            whileTap={{ scale: 1.2, 
+            whileTap={{ scale: 0.7, 
               transition: { duration: 0.1 }
              }}
           >
